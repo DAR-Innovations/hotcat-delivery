638836c7be4d14c2f47ab76cad335959676e1c4c
DiarCode <diar_beg64@mail.ru>
2022-10-01 23:38:54 +0600

    menu food modal refactored

--- src/components/MenuFoodList/MenuFoodModal.tsx
+++ src/components/MenuFoodList/MenuFoodModal.tsx
@@ -53,13 +53,15 @@ const MenuFoodModal = () => {
     <div
       onClick={handleCloseModal}
       className={`${
-        isModalActive ? "block" : "hidden"
-      } absolute top-0 right-0 left-0 bottom-0 w-screen h-screen bg-black bg-opacity-60 backdrop-blur-sm`}
+        isModalActive ? "visible opacity-100" : "invisible opacity-0"
+      } absolute top-0 right-0 left-0 bottom-0 w-screen h-screen bg-black bg-opacity-60 backdrop-blur-sm duration-300 transition-all`}
     >
-      <div className="relative  h-full flex items-center justify-center">
+      <div className="h-full flex items-center justify-center">
         <div
           onClick={e => e.stopPropagation()}
-          className="max-w-[500px] bg-white rounded-xl relative"
+          className={`${
+            isModalActive ? "visible opacity-100" : "invisible opacity-0"
+          } max-w-[360px] sm:max-w-[500px] bg-white rounded-xl relative`}
         >
           <div>
             <picture>
@@ -82,28 +84,30 @@ const MenuFoodModal = () => {
               price={selectedFood?.price}
             />
 
-            <p className="mb-6">{selectedFood?.description}</p>
+            <p className="text-sm sm:text-base mb-6">
+              {selectedFood?.description}
+            </p>
 
             <div className="flex item-center gap-x-2 justify-between">
-              <div className="flex items-center gap-x-5 border-2 border-black px-4 py-2 rounded-lg">
+              <div className="flex items-center gap-x-5 border-2 border-black px-2 py-1 sm:px-4 sm:py-2 rounded-lg">
                 <button
                   onClick={handleSubtractCount}
                   className="rounded-md bg-gray-100 p-1"
                   disabled={isMinusButtonDisabled}
                 >
-                  <MinusSolid className="w-4 h-4" />
+                  <MinusSolid className="w-3 h-3 sm:w-4 sm:h-4" />
                 </button>
                 <p>{foodCount}</p>
                 <button
                   onClick={handleAddCount}
                   className="rounded-md bg-gray-100 p-1"
                 >
-                  <PlusSolid className="w-4 h-4" />
+                  <PlusSolid className="w-3 h-3 sm:w-4 sm:h-4" />
                 </button>
               </div>
 
               <div className="w-full">
-                <button className="w-full px-5 py-4 bg-black hover:bg-gray-700 duration-200 transition-all text-white rounded-lg font-medium">
+                <button className="w-full p-3 sm:px-5 sm:py-4 bg-black hover:bg-gray-700 duration-200 transition-all text-white rounded-lg font-normal sm:font-medium">
                   <div className="flex items-center justify-center gap-x-4">
                     <p>Add order</p>
                     <PriceWithSymbol price={totalPrice} fill="white" />
