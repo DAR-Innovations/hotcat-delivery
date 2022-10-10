a3f7768ebdbbf6bab232e970d35c9bcb41779cec
DiarCode <diar_beg64@mail.ru>
2022-10-11 01:29:17 +0600

    orders hisoty ui refactored

--- src/pages/profile/orders/index.tsx
+++ src/pages/profile/orders/index.tsx
@@ -20,38 +20,40 @@ const ProfileOrdersPage: NextPage = () => {
     { enabled: userId !== null }
   );
 
-  console.log(orders);
+  const renderedOrders = orders?.map((order, index) => {
+    const isEven = index % 2 === 0;
 
-  const renderedOrders = orders?.map(order => (
-    <div
-      key={order.id}
-      className="grid grid-cols-3 sm:grid-cols-4 gap-x-20 w-full sm:w-[800px] cursor-pointer rounded-xl items-center justify-between py-4 sm:py-7 px-4 hover:bg-gray-100 duration-200 transition-all"
-    >
-      <div className="col-span-2 sm:col-span-3 grid grid-cols-2 items-center gap-x-6 gap-y-1">
-        <div className="col-span-2 sm:col-span-1">
-          <p className="font-semibold sm:text-lg leading-none truncate ">
-            {order.restaurant.name}
-          </p>
+    return (
+      <div
+        key={order.id}
+        className={`${
+          isEven && "bg-gray-100"
+        } grid grid-cols-3 sm:grid-cols-4 gap-x-20 w-full sm:w-[800px] rounded-xl items-center justify-between py-3 px-4`}
+      >
+        <div className="col-span-2 sm:col-span-3 grid grid-cols-2 items-center gap-x-6 gap-y-1">
+          <div className="col-span-2 sm:col-span-1">
+            <p className="font-semibold sm:text-lg leading-none truncate ">
+              {order.restaurant.name}
+            </p>
+          </div>
+          <div className="col-span-2 sm:col-span-1">
+            <p className="text-gray-400 font-normal text-sm sm:text-base">
+              2022-01-15
+            </p>
+          </div>
         </div>
-        <div className="col-span-2 sm:col-span-1">
-          <p className="text-gray-400 font-normal text-sm sm:text-base">
-            2022-01-15
-          </p>
-        </div>
-      </div>
-      <div className="col-span-1 sm:col-span-1 flex items-center justify-end gap-1 text-base">
-        <div className="flex items-center gap-2">
-          <p>KZT</p>
-          <p>{order.totalPrice}</p>
-        </div>
-        <div>
+        <div className="col-span-1 sm:col-span-1 flex items-center justify-end gap-1 text-base">
+          <div className="flex items-center gap-2">
+            <p>KZT</p>
+            <p>{order.totalPrice}</p>
+          </div>
+          {/* <div>
           <NextSolid filledColor="rgb(156 163 175)" className="w-4 h-4" />
+        </div> */}
         </div>
       </div>
-    </div>
-  ));
-
-  //TODO: CREATE ORDERS HISTORY EXCERPT
+    );
+  });
 
   return (
     <Layout title={PAGES_LINKS.ORDERS.name}>
@@ -60,7 +62,7 @@ const ProfileOrdersPage: NextPage = () => {
           Profile / Order history
         </h1>
 
-        <main className="flex flex-col items-center mt-4">
+        <main className="flex flex-col items-center gap-y-3 mt-4">
           {renderedOrders}
         </main>
       </div>
